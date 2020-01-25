e14b987395ef7d98699ca118547d8126de91b5f9
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-01-25 13:14:07 -0300

    feature: create metod to add users

--- src/app.js
+++ src/app.js
@@ -5,22 +5,22 @@ import path from 'path';
 import 'express-async-errors';
 import routes from './routes';
 
-// import './database';
+import './database';
 
 class App {
-  constructor() {
-    this.server = express();
+	constructor() {
+		this.server = express();
 
-    this.middlewares();
-    this.routes();
-  }
+		this.middlewares();
+		this.routes();
+	}
 
-  middlewares() {
-    this.server.use(express.json());
-  }
+	middlewares() {
+		this.server.use(express.json());
+	}
 
-  routes() {
-    this.server.use(routes);
-  }
+	routes() {
+		this.server.use(routes);
+	}
 }
 export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 // import dos controllers
 import UserController from './app/controllers/UserController';
+
 // constantes
 const routes = new Router();
 
