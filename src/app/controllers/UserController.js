3e1f6376db24243d5ce1fa31236140ecf0efa9e7
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-01-25 13:55:59 -0300

    release: add method to valid data input from user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
 	async store(req, res) {
+		const schema = Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string()
+				.email()
+				.required(),
+			password: Yup.string()
+				.required()
+				.min(6),
+		});
+
+		/* check if datas sendind from method post is agree with schema validations */
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Validations fails.' });
+		}
 		const usersExist = await User.findOne({ where: { email: req.body.email } });
 
 		/* check if user exist before save */
