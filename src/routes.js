e387487dd5af3e20276b86302d73757fa1c813ca
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-01-25 12:36:49 -0300

    realease: create basic config project and migration refer users

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+// import dos controllers
+import UserController from './app/controllers/UserController';
+// constantes
+const routes = new Router();
+
+// Rotas
+routes.post('/users', UserController.store);
+
+export default routes;
